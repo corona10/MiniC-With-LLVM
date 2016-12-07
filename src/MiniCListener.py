2ac507933bc8dd2e4646b95e3a268fa1b5411f3e
Lee SangHee <sanghee.lee1992@gmail.com>
2016-12-07 10:03:53 +0900

    Init MiniCPrinter

--- src/MiniCListener.py
+++ src/MiniCListener.py
@@ -3,10 +3,11 @@ from antlr4 import *
 
 # This class defines a complete listener for a parse tree produced by MiniCParser.
 class MiniCListener(ParseTreeListener):
+    def __init__(self):
+        self.prop={}
 
     # Enter a parse tree produced by MiniCParser#program.
     def enterProgram(self, ctx):
-	print ctx.getText()
         pass
 
     # Exit a parse tree produced by MiniCParser#program.
@@ -74,6 +75,20 @@ class MiniCListener(ParseTreeListener):
 
     # Exit a parse tree produced by MiniCParser#stmt.
     def exitStmt(self, ctx):
+        stmt = ""
+        if ctx.getChildCount > 0:
+            if ctx.expr_stmt is not None:
+                print ctx.getText()
+                stmt += self.prop[ctx.expr_stmt()]
+            elif ctx.compound_stmt is not None:
+                stmt += self.prop[ctx.compound_stmt()]
+            elif ctx.if_stmt is not None:
+                stmt += self.prop[ctx.if_stmt]
+            elif ctx.while_stmt is not None:
+                stmt += self.prop[ctx.while_stmt]
+            else:
+                stmt += self.prop[ctx.return_stmt]
+        self.prop[ctx]=stmt
         pass
 
 
@@ -83,6 +98,13 @@ class MiniCListener(ParseTreeListener):
 
     # Exit a parse tree produced by MiniCParser#expr_stmt.
     def exitExpr_stmt(self, ctx):
+        stmt = ""
+        if ctx.getChildCount() == 2:
+            stmt +=" "
+            stmt +=self.prop[ctx.expr()]
+            stmt +=ctx.getChild(1).getText()
+            stmt +="\n"
+        self.prop[ctx]=stmt
         pass
 
 
@@ -137,6 +159,54 @@ class MiniCListener(ParseTreeListener):
 
     # Exit a parse tree produced by MiniCParser#expr.
     def exitExpr(self, ctx):
+        expr=""
+        print ctx.getText()
+        if ctx.getChildCount() > 0:
+            if ctx.getChildCount() == 1:
+                expr+=ctx.getChild(0).getText()
+            elif ctx.getChildCount() == 2:
+                expr+=ctx.getChild(0).getText()
+                expr+=self.prop[ctx.expr(0)]
+            elif ctx.getChildCount() == 3:
+                if ctx.getChild(0).getText() == "(":
+                    expr+=ctx.getChild(0).getText()
+                    expr+=self.prop[ctx.expr(0)]
+                    expr+=ctx.getChild(2).getText()
+                elif ctx.getChild(1).getText() == "=":
+                    expr+=ctx.getChild(0).getText()
+                    expr+=" "
+                    expr+=ctx.getChild(1).getText()
+                    expr+=" "
+                    expr+=self.prop[ctx.expr(0)]
+                else: #binary
+                    expr+=self.prop[ctx.expr(0)]
+                    expr+=" "
+                    expr+=ctx.getChild(1).getText()
+                    expr+=" "
+                    expr+=self.prop[ctx.expr(1)]
+            elif ctx.getChildCount() == 4:
+                expr+=ctx.getChild(0).getText()
+                if ctx.args() is not None:
+                    expr+=ctx.getChild(1).getText()
+                    expr+=self.prop[ctx.args()]
+                else:
+                    expr+=ctx.getChild(1).getText
+                    expr+=self.prop[ctx.expr(0)]
+                expr+=ctx.getChild(3).getText()
+            else:
+                expr+=ctx.getChild(0).getText()
+                expr+=ctx.getChild(1).getText()
+                expr+=self.prop[ctx.expr(0)]
+                expr+=ctx.getChild(3).getText()
+                expr+=" "
+                expr+=ctx.getChild(4).getText()
+                expr+=" "
+                expr+=self.prop[ctx.expr(1)]
+            self.prop[ctx]=expr
+
+             
+            
+	print self.prop[ctx]
         pass
 
 
@@ -146,6 +216,15 @@ class MiniCListener(ParseTreeListener):
 
     # Exit a parse tree produced by MiniCParser#args.
     def exitArgs(self, ctx):
+        args=""
+        if ctx.getChildCount() >=0:
+            for i in range(ctx.getChildCount()):
+                if i%2==0:
+                    args+=self.prop[ctx.expr(i/2)]
+                else:
+                    args+=ctx.getChild(i).getText()
+                    args+=" "
+        self.prop[ctx]=args
         pass
 
 
